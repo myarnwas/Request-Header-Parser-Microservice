6b7f34c7c845041145ee1a983ba5c3f71b74129b
myarnwas <myarnwas@gmail.com>
2025-08-04 08:35:21 +0300

    solve project2

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const app = express();
 
-// الصفحة الرئيسية
 app.get('/', (req, res) => {
-  res.send('Welcome to the Request Header Parser Microservice');
+  res.send('Request Header Parser Microservice');
 });
 
-// نقطة النهاية المطلوبة من FreeCodeCamp
 app.get('/api/whoami', (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const language = req.headers['accept-language'];
-  const software = req.headers['user-agent'];
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || '';
+  const language = req.headers['accept-language'] || '';
+  const software = req.headers['user-agent'] || '';
 
   res.json({
-    ipaddress: ip.split(',')[0],
-    language,
-    software
+    ipaddress: ip.split(',')[0],   // in case of multiple IPs
+    language: language,
+    software: software
   });
 });
 
